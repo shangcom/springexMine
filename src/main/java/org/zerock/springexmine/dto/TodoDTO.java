1c801e55a5fba3ff069263da76a239d4a8495418
shangcom <dndbs91@gmail.com>
2024-05-12 01:51:24 +0900

    p279부터 정리하기

---
+++ src/main/java/org/zerock/springexmine/dto/TodoDTO.java
@@ -0,0 +1,26 @@
+package org.zerock.springexmine.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@ToString
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoDTO {
+
+    private Long tno;
+
+//    @NotEmpty
+    private String title;
+
+//    @Future
+    private LocalDate dueDate;
+
+    private boolean finished;
+
+//    @NotEmpty
+    private String writer;
+}
